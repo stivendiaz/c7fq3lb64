f214b07788122346193d61e18ee59693ecf7c7a8
Diaz <david.stiven.diaz@accenture.com>
2018-10-02 15:12:31 -0500

    Añadimos funcionalidad

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,12 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+     
+     if params[:concept].present? and params[:category_id].present?
+      @expenses = Expense.where("concept LIKE ? AND category_id=? ", "%#{params[:concept]}%", params[:category_id]).order("date DESC")
+     elsif params[:concept].present? or params[:category_id].present?
+      @expenses = Expense.where("concept LIKE ?", "%#{params[:concept]}%").or(Expense.where("category_id=?", params[:category_id])).order("date DESC")
+     else
+  	  @expenses = Expense.order("date DESC")
+  	end   
   end
 end
