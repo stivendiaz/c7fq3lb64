22c1c70d80aa853dfa7dc529ca1aebace5ce415d
Diaz <david.stiven.diaz@accenture.com>
2018-10-02 20:56:11 -0500

    arreglamos filtro

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,12 +1,11 @@
 class ExpensesController < ApplicationController
   def index
-     
-     if params[:concept].present? and params[:category_id].present?
-      @expenses = Expense.where("concept LIKE ? AND category_id=? ", "%#{params[:concept]}%", params[:category_id]).order("date DESC")
-     elsif params[:concept].present? or params[:category_id].present?
-      @expenses = Expense.where("concept LIKE ?", "%#{params[:concept]}%").or(Expense.where("category_id=?", params[:category_id])).order("date DESC")
-     else
-  	  @expenses = Expense.order("date DESC")
-  	end   
+    @expenses = Expense.order("date DESC")
+    if params[:concept].present?
+      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+     end
+     if params[:category_id].present?
+      @expenses = @expenses.where("category_id = ?", params[:category_id])
+     end
   end
 end
